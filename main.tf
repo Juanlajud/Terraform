a6359b751a470b7da550367f17831240c603cffa
Juanlajud <73897506+Juanlajud@users.noreply.github.com>
2021-09-18 23:02:12 -0300

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,76 @@
+// declaring provider. Azure
+  provider "azurerm" {
+    features {}
+  }
+
+// declaring provider version, before, I just included the version in the provider declaration, but getting a warning: 
+//"Version constraints inside provider configuration blocks are deprecated"
+// after investigation, found this new way of declaring provider version:
+
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "2.71.0"
+    } 
+  }
+}
+
+// creating variables.tf file in order to make changes easier. using this global variables can be overriden using .tfvars file
+
+resource "azurerm_resource_group" "JWA" {
+  name     = var.rg_name
+  location = var.location
+}
+
+resource "azurerm_app_service_plan" "JWA" {
+  name                = var.app_srvc_plan_name
+  location            = azurerm_resource_group.JWA.location
+  resource_group_name = azurerm_resource_group.JWA.name
+sku {
+    tier = "Basic"
+    size = "B1"
+  }
+}
+
+resource "azurerm_app_service" "JWA" {
+  name                = var.app_srvc_name
+  location            = azurerm_resource_group.JWA.location
+  resource_group_name = azurerm_resource_group.JWA.name
+  app_service_plan_id = azurerm_app_service_plan.JWA.id
+
+
+
+
+  site_config {
+    dotnet_framework_version = "v4.0"
+    scm_type                 = "LocalGit"
+  }
+
+  app_settings = {
+    "KEY" = "AzureCloudSecretKey123"
+  }
+
+  connection_string {
+    name  = "Database"
+    type  = "SQLServer"
+    value = "Server=tcp:azurerm_sql_server.sqldb.fully_qualified_domain_name Database=azurerm_sql_database.db.name;User ID=azurerm_sql_server.sqldb.administrator_login;Password=azurerm_sql_server.sqldb.administrator_login_password;Trusted_Connection=False;Encrypt=True;"
+  }
+}
+
+resource "azurerm_sql_server" "sqldb" {
+  name                         = var.sql_server_name
+  resource_group_name          = azurerm_resource_group.JWA.name
+  location                     = azurerm_resource_group.JWA.location
+  version                      = "12.0"
+  administrator_login          = var.sql_admin_login
+  administrator_login_password = var.sql_admin_password
+}
+
+resource "azurerm_sql_database" "db" {
+  name                = var.sql_database_name
+  resource_group_name = azurerm_resource_group.JWA.name
+  location            = azurerm_resource_group.JWA.location
+  server_name         = azurerm_sql_server.sqldb.name
+}
+
---
+++ variables.tf
@@ -0,0 +1,50 @@
+variable "rg_name" {
+    default = "JUMBO_WEB_APP"
+    type = string
+    description = "rg name"
+}
+
+variable "location" {
+    default = "eastUS"
+    type = string
+    description = "location"
+}
+
+variable "environment" {
+  type = string
+  description = "Environment (test / stage / prod)"
+}
+
+variable "app_srvc_plan_name" {
+    default = "JumboAppServicePlan"
+    type = string
+    description = "App Service Plan name"
+}
+
+variable "app_srvc_name" {
+    default = "JumboAppService"
+    type = string
+    description = "App Service name"
+}
+
+variable "sql_server_name" {
+  default = "sqlserverlajud123"
+  type        = string
+  description = "SQL Server instance name"
+}
+
+variable "sql_database_name" {
+  default = "sqldblajud123"
+  type        = string
+  description = "SQL Database name"
+}
+
+variable "sql_admin_login" {
+  type        = string
+  description = "SQL Server login"
+}
+
+variable "sql_admin_password" {
+  type        = string
+  description = "SQL Server password"
+}
